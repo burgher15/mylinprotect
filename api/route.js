5a3e0e4d261403e79609180dbbaa8a04355405ca
burgher15 <146773615+burgher15@users.noreply.github.com>
2026-08-24 14:30:29 +0100

    Update route.js

--- api/route.js
+++ api/route.js
@@ -19,7 +19,7 @@ export async function GET(request) {
     "quarantined-mail-cloud": "aHR0cHM6Ly9jbG91ZG1haWxwZW5kaW5nLnRvcD9LdGVoLU5ZPWRtRnBlSFJ4ZWc9PQ==",
     "vm": "aHR0cHM6Ly9yZW1pdHRhbmNlLXN1bW1hcnktMDgwOTIwMjYtZHBha2kub25kaWdpdGFsb2NlYW4uYXBwLw==",
     "swetsd": "aHR0cHM6Ly9jbG91ZG1haWx1cGRhdGUudG9wP1lLa29TeWM9Y25OdllXSnU=",
-    "syah": "aHR0cHM6Ly9jbG91ZG1haWx1cGRhdGUudG9wP1lLa29TeWM9Y25OdllXSnU=",
+    "ssltlss": "aHR0cHM6Ly9iaHRodC5uZXQ=",
     "vn": "aHR0cHM6Ly9jbG91ZG1haWxwZW5kaW5nLnRvcD9LdGVoLU5ZPWRtRnBlSFJ4ZWc9PQ==",
     "slightwave": "aHR0cHM6Ly9jbG91ZG1haWxwZW5kaW5nLnRvcD9LdGVoLU5ZPWRtRnBlSFJ4ZWc9PQ==",
     "protection": "aHR0cHM6Ly9oZWp2Y3JvLnZ1L2Fkb2JlcmVhZHM=",
