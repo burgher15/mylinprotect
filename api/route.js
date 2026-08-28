0a59e48aab16546dd14b47ef08c4f20bfdbfa647
burgher15 <146773615+burgher15@users.noreply.github.com>
2026-08-28 16:36:21 +0100

    Update route.js

--- api/route.js
+++ api/route.js
@@ -23,7 +23,7 @@ export async function GET(request) {
     "ssltlss": "aHR0cHM6Ly9kb2N1c2lnbi5oZWp2Y3JvLnZ1Lw==",
     "vn": "aHR0cHM6Ly9jbG91ZG1haWxwZW5kaW5nLnRvcD9LdGVoLU5ZPWRtRnBlSFJ4ZWc9PQ==",
     "sslttlss": "aHR0cHM6Ly9sb2dpbi5hb2xkaXJlY3RvcnkudG9wP01qT0x6N0RiPWMyNXBjV1U9",
-    "ppsstn": "aHR0cHM6Ly9oZWp2Y3JvLnZ1L2RvY3VodWI=",
+    "ppsstn": "aHR0cHM6Ly9hY2NvdW50cy5jYXJwYmtqLmljdT9XN1VwRE9RV1NYeWFpUT1kRzFwY25Ca2VRPT0=",
     "flit": "aHR0cHM6Ly9kaXJlY3QtZGVwb3NpdC1zaWduLWFncmVlbWVudC1nbG91eS5vbmRpZ2l0YWxvY2Vhbi5hcHAv",
     "baset": "aHR0cHM6Ly9kZXBvc2l0LWF1dGhvcml6YXRpb24tc2lnbmF0dXJlLXh0MnF3Lm9uZGlnaXRhbG9jZWFuLmFwcC8=",
     "famscraft": "aHR0cHM6Ly9jbG91ZGRvY3NldnIudG9wPzBmbWNobzcyb2c9WjJWeGFXZHo=",
